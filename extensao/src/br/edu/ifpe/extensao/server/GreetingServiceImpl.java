ec8a7e962c0e2decba3221a7245f827093068c05
alexsandro-matias <matiasalexsandro@gmail.com>
2023-07-03 18:44:22 -0300

    Aula a ser vista - aula 3756 gwt Conhecendo a estrutura da aplicacao gwt

--- extensao/src/br/edu/ifpe/extensao/server/GreetingServiceImpl.java
+++ extensao/src/br/edu/ifpe/extensao/server/GreetingServiceImpl.java
@@ -11,16 +11,18 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 {
 	public String greetServer(String input) throws IllegalArgumentException
 	{
-		// Verify that the input is valid. 
+		// Verify that the input is valid.
 		if (!FieldVerifier.isValidName(input))
 		{
-			// If the input is not valid, throw an IllegalArgumentException back to
+			// If the input is not valid, throw an IllegalArgumentException back
+			// to
 			// the client.
 			throw new IllegalArgumentException("Name must be at least 4 characters long");
 		}
 		String serverInfo = getServletContext().getServerInfo();
 		String userAgent = getThreadLocalRequest().getHeader("User-Agent");
-		// Escape data from the client to avoid cross-site script vulnerabilities.
+		// Escape data from the client to avoid cross-site script
+		// vulnerabilities.
 		input = escapeHtml(input);
 		userAgent = escapeHtml(userAgent);
 		return "Hello, " + input + "!<br><br>I am running " + serverInfo + ".<br><br>It looks like you are using:<br>"
@@ -31,7 +33,8 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 	 * Escape an html string. Escaping data received from the client helps to
 	 * prevent cross-site script vulnerabilities.
 	 * 
-	 * @param html the html string to escape
+	 * @param html
+	 *            the html string to escape
 	 * @return the escaped string
 	 */
 	private String escapeHtml(String html)
